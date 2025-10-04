bb2b2d3dc50ccf0409b9b30a5a1d3d2f66c9bd63
berke-volkan <berkevolkan0909878909@gmail.com>
2025-10-04 18:09:04 +0300

    blocks

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 import requests
 import json
 import datetime
+import math
 # Install the Slack app and get xoxb- token in advance
+
 
 app = App(token=OBB)
 
@@ -42,7 +44,7 @@ def join(message,say):
 
 @app.message("-token")
 def explore(message,say):
-    say("buying token")
+    say(message[0])
     with open("tokens.json","r") as f:
        data = json.load(f)
        entry=data["tokens"][0]["price"]
@@ -52,7 +54,7 @@ def explore(message,say):
 
 
 @app.message("-recalc")
-def explore(message,say):
+def recalc(message,say):
     say("recalcing token price")
     resp=requests.get("https://hackatime.hackclub.com/api/v1/users/kzlpndx/stats?start_date=2025-09-03&end_date=2025-10-03")
     resp=resp.json()
@@ -65,10 +67,69 @@ def explore(message,say):
        price=data["tokens"][0]["price"]
        pos=data["tokens"][0]["holders"][0]["U07SU9F50MT"]["bal"]
        change=(price-entry)*pos
-       say(f"UPDATE: New token Price: {price} \n Change On your Balance {change}")
+       change_percent=round((multiplier-1),2)*100
+       say(f"UPDATE: New token Price: {price} \n Change On your Balance {change} \n Change(%): %{change_percent}")
+@app.message("explore") 
+def explore(message,say):
+    best_roi={"24h":0}
+    with open("tokens.json","r") as f:
+        tokens=json.load(f)
+        for token in tokens["tokens"]:
+            if token["24h"]>best_roi["24h"]:
+                best_roi=token
+    name=best_roi["token"]
+    change=best_roi["24h"]
+    price=best_roi["price"]
+    say(f"Best token in 24h is: {name} \n24H Change: {change} \nTrade Price: {price} \n\n Created by: /- I need to add a created_by thingy after changing from message event to command -/")
 
-     
-
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+    with open("users.json","r") as f:
+            data = json.load(f)
+            for i in data["users"]:
+                if (i["slack_id"]==event["user"]):
+                    user=i
+    token=user["holdings"]
+    markdown=""
+    for x in token:
+        name=x["token"]
+        bal=x["bal"]
+        price=x["price"]
+        a=f"{name}    | {bal}      | H${price} \n"    
+        markdown+=a           
+    try:
+        # Call views.publish with the built-in client
+        client.views_publish(
+            # Use the user ID associated with the event
+            user_id=event["user"],
+            # Home tabs must be enabled in your app configuration
+            view={
+    "type":"home",
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*Hey! Welcome to code or short :)*"
+			}
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text":                    "```"
+                    "Coin   | Amount   | Value (H$)\n"
+                    f"{markdown}"
+                    "```"
+			}
+		},
+	]
+})
+    except Exception as e:
+        logger.error(f"Error publishing home tab: {e}")
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, APP)
